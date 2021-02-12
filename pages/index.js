59ad60b8efe2080a5f7a9a98fb1367baaeb6d082
realkelvinperez <realkelvinperez@gmail.com>
2021-02-12 05:24:42 -0500

    updaed packages and created header component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import styled from 'styled-components'
+
+const HeaderWrap = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Kelvin Perez Web Developer Portfolio | UI/UX & Full-Stack | Miami, FL | Figma PHP Laravel React SQL</title>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+        <HeaderWrap>
+            <Header />
+        </HeaderWrap>
     </div>
   )
 }
